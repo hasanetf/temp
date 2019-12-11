c1a650e07d7b4fea6a6623f6e0da212bd1ce655e
hasan <hasanredzo@gmail.com>
2019-12-11 14:49:28 +0100

    test

--- php/dbCreate.php
+++ php/dbCreate.php
@@ -3,9 +3,7 @@
 
 $db = new SQLite3('control4db') or die('Unable to open database');
 
-$query = "CREATE TABLE IF NOT EXISTS users (
-  username STRING PRIMARY KEY, 
-  password STRING)";
+$query = "CREATE TABLE IF NOT EXISTS users (username STRING PRIMARY KEY, password STRING)";
 
 $db->exec($query) or die('Create db failed');
 
