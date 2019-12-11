4a46a65f00b3a03e4aa4f1914f91db0390d101ed
hasan <hasanredzo@gmail.com>
2019-12-11 16:01:10 +0100

    test

--- php/signup.php
+++ php/signup.php
@@ -10,7 +10,7 @@ $count = 0;
 $query = "SELECT * FROM C4users WHERE username='$myusername' and passwrd='$mypassword'";
 $res = $db->exec($query) or die('Select users from db failed');
 
-$query = "INSERT INTO users(username, passwrd) VALUES('$myusername', '$mypassword')";
+$query = "INSERT INTO C4users(username, passwrd) VALUES('$myusername', '$mypassword')";
 $db->exec($query) or die('Add user into db failed');
 
 
