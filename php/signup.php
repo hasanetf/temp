bf7e50b256df75b2a3918871f895022ec962661d
hasan <hasanredzo@gmail.com>
2019-12-11 15:57:38 +0100

    test

--- php/signup.php
+++ php/signup.php
@@ -10,22 +10,9 @@ $count = 0;
 $query = "SELECT * FROM C4users WHERE username='$myusername' and passwrd='$mypassword'";
 $res = $db->exec($query) or die('Select users from db failed');
 
-$check = empty($res);
+$query = "INSERT INTO users(username, passwrd) VALUES('$myusername', '$mypassword')";
+$db->exec($query) or die('Add user into db failed');
 
-echo "Is it empty {$check}\n";
-
-if(!empty($res)){
-  $count = count($res);
-}
-
-echo "Is it empty $res  {$count}\n";
-
-if($count > 0){
-  echo "User exists";
-}else{
-  $query = "INSERT INTO users(username, passwrd) VALUES('$myusername', '$mypassword')";
-  $db->exec($query) or die('Add user into db failed');
-}
 
 $db->close();
 ?>
