74136da93e7695a784749163394077cfab891db0
hasan <hasanredzo@gmail.com>
2019-12-11 15:44:08 +0100

    test

--- php/signup.php
+++ php/signup.php
@@ -4,8 +4,8 @@
 $db = new SQLite3('control4db') or die('Unable to open database');
 
 $query = "CREATE TABLE IF NOT EXISTS C4users (id INTEGER PRIMARY KEY,
-username STRING, 
-passwrd STRING)";
+username TEXT NOT NULL, 
+passwrd TEXT NOT NULL)";
 
 $db->exec($query) or die('Create db failed');
 
@@ -15,6 +15,11 @@ $count = 0;
 
 $query = "SELECT * FROM C4users WHERE username='$myusername' and passwrd='$mypassword'";
 $res = $db->exec($query) or die('Select users from db failed');
+
+$check = empty($res);
+
+echo "Is it empty {$check}\n";
+
 if(!empty($res)){
   $count = count($res);
 }
