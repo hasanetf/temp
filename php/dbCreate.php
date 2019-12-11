af7a2ccc21f57d88584329a10cd49f545431ba74
hasan <hasanredzo@gmail.com>
2019-12-11 15:52:29 +0100

    test

--- php/dbCreate.php
+++ php/dbCreate.php
@@ -4,10 +4,11 @@
 $db = new SQLite3('control4db') or die('Unable to open database');
 
 $query = "CREATE TABLE IF NOT EXISTS C4users (id INTEGER PRIMARY KEY,
-username STRING, 
-passwrd STRING)";
+username TEXT NOT NULL, 
+passwrd TEXT NOT NULL)";
 
 $db->exec($query) or die('Create db failed');
+
 
 $db->close();
 ?>
--- php/signup.php
+++ php/signup.php
@@ -3,12 +3,6 @@
 
 $db = new SQLite3('control4db') or die('Unable to open database');
 
-$query = "CREATE TABLE IF NOT EXISTS C4users (id INTEGER PRIMARY KEY,
-username TEXT NOT NULL, 
-passwrd TEXT NOT NULL)";
-
-$db->exec($query) or die('Create db failed');
-
 $myusername=$_POST['username']; 
 $mypassword=$_POST['password'];
 $count = 0;
