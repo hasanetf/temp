c51efbad8437fb268182cfb16d2244dfdb4fd83e
hasan <hasanredzo@gmail.com>
2019-12-11 17:36:36 +0100

    test

--- php/dbCreate.php
+++ php/dbCreate.php
@@ -5,7 +5,7 @@ $db = new SQLite3('control4db') or die('Unable to open database');
 
 $query = "CREATE TABLE IF NOT EXISTS C4users (id INTEGER PRIMARY KEY,
 username TEXT NOT NULL, 
-passwrd TEXT NOT NULL)";
+passwrd TEXT NOT NULL);";
 
 $db->exec($query) or die('Create db failed');
 
--- php/deldb.php
+++ php/deldb.php
@@ -2,7 +2,7 @@
 @session_start();
 
 $db = new SQLite3('control4db') or die('Unable to open database');
-$query = "DROP TABLE users";
+$query = "DROP TABLE users;";
 
 $db->exec($query) or die('Delete table from db failed');
 
