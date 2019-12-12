0d9132761a115097eddac4e220b33681a2832c1f
hasan <hasanredzo@gmail.com>
2019-12-12 12:17:25 +0100

    test

--- php/signup.php
+++ php/signup.php
@@ -8,11 +8,11 @@ $mypassword=$_POST['password'];
 $count = 0;
 echo "Values are $myusername, $mypassword";
 
-$query = "SELECT * FROM C4users WHERE username=$myusername and passwrd=$mypassword";
+$query = "SELECT * FROM C4users WHERE username='$myusername' and passwrd='$mypassword'";
 echo "QUERY 1 $query";
 $res = $db->exec($query) or die('Select users from db failed');
 
-$query = "INSERT INTO C4users VALUES (NULL, $myusername, $mypassword)";
+$query = "INSERT INTO C4users VALUES (NULL, '$myusername', '$mypassword')";
 echo "QUERY 2 $query";
 $db->exec($query) or die('Add user into db failed');
 
