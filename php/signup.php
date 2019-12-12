27854f274c7b49632ccd79d2cbe2510cf9082cbd
hasan <hasanredzo@gmail.com>
2019-12-12 10:21:27 +0100

    test

--- php/signup.php
+++ php/signup.php
@@ -13,7 +13,7 @@ $res = $db->exec($query) or die('Select users from db failed');
 $query = "INSERT INTO C4users(username, passwrd) VALUES('$myusername', '$mypassword');";
 $db->exec($query) or die('Add user into db failed');
 
-header("location:index.php");
+header("location:../index.php");
 
 
 $db->close();
