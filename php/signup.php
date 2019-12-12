e9d61874c9d903ced74ee8e699e02342703fb063
hasan <hasanredzo@gmail.com>
2019-12-12 14:26:40 +0100

    test

--- php/signup.php
+++ php/signup.php
@@ -11,18 +11,22 @@ $statement = $db->prepare('SELECT * FROM C4users WHERE username=:id1;');
 $statement->bindValue(':id1', $myusername);
 $result = $statement->execute();
 
-$count = count($result);
+#$count = count($result);
+$total = $result[0];
+echo "Total number of rows " . $total;
 
 if($count > 0){
-  $db->close();
-  header("location:../index.php?usr=1");
+  $_SESSION['passwrd'] = -1;
+
 }else{
   $statement = $db->prepare("INSERT INTO C4users VALUES (NULL, 'id1','id2', 3);");
   $statement->bindValue(':id1', $myusername);
   $statement->bindValue(':id2', $mypassword);
   $result = $statement->execute();
+  $_SESSION['passwrd'] = "-1";
+  $_SESSION['username'] = "-1";
 }
 
 $db->close();
-header("location:../index.php?usr=0");
+//header("location:../index.php");
 ?>
