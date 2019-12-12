aaafbdc486f510618e3d7d4752f7d20ed949aa3a
hasan <hasanredzo@gmail.com>
2019-12-12 17:38:01 +0100

    test

--- php/signup.php
+++ php/signup.php
@@ -3,6 +3,8 @@
 
 $db = new SQLite3('/www/temp/control4.db') or die('Unable to open database');
 
+$isUser = "0";
+
 $myusername=$_POST['username']; 
 $mypassword=$_POST['password'];
 
@@ -10,22 +12,22 @@ $statement = $db->prepare('SELECT count(*) FROM C4users WHERE username=?;');
 $statement->bindValue(1, $myusername, SQLITE3_TEXT);
 $result = $statement->execute();
 
-$row = $result->fetchArray() ;
+$row = $result->fetchArray();
 
 $total = $row[0];
 echo "Total number of rows " . $total;
 
 if($total > 0){
-  $_SESSION['passwrd'] = -1;
+  $isUser = "-1";
 
 }else{
   $statement = $db->prepare("INSERT INTO C4users VALUES (NULL, ?,?, 3);");
   $statement->bindValue(1, $myusername, SQLITE3_TEXT);
   $statement->bindValue(2, $mypassword, SQLITE3_TEXT);
   $result = $statement->execute();
-  $_SESSION['passwrd'] = "1";
+  $isUser = "1";
 }
 
-$db->close();
+$_SESSION['passwrd'] = $isUser;
 header("location:../index.php");
 ?>
