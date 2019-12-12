ad4f89ca832461d06a6e7bc306a6e8161bcdd11f
hasan <hasanredzo@gmail.com>
2019-12-12 18:08:55 +0100

    test

--- php/LogReg.php
+++ php/LogReg.php
@@ -13,6 +13,8 @@
             echo "<legend>Personal information:</legend>";
         }
     ?>
+    <div class="login">
+        <h2> Login </h2>
         <form action="php/signup.php" method="post">
             Username:<br>
             <input type="text" name="username">
@@ -20,8 +22,20 @@
             Password:<br>
             <input type="text" name="password">
             <br><br>
-            <input type="submit" value="Signup">
+            <input type="submit" value="Join now">
         </form>
+    </div>
+    <div class="join">
+        <h2> Register </h2>
+        <form action="php/signup.php" method="post">
+            Username:<br>
+            <input type="text" name="username">
+            <br>
+            Password:<br>
+            <input type="text" name="password">
+            <br><br>
+            <input type="submit" value="Join now">
+        </form>
+    </div>
     </fieldset>
 </div>
-
