7224580255080ebd94411879b4af2b4bc1522659
hasan <hasanredzo@gmail.com>
2019-12-13 17:35:43 +0100

    test

--- php/brwFiles.php
+++ php/brwFiles.php
@@ -1,11 +1,7 @@
 <?php
     @session_start();
     $dir    = '/mnt/sda1';
-    echo '<table id="tbl">
-            <caption> USB Flash Drive Contents </caption>   
-                <tr id="tbl">  
-                    <th id="tbl"> Path </th>
-                </tr> ';
+    echo '<table id="usbtbl">';
     if(!isset($_SESSION['dir_path'])){
         $_SESSION['dir_path'] = $dir;
     }else{
