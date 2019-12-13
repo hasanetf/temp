6229480a404051f603221a4ccb1840f3f1afa793
hasan <hasanredzo@gmail.com>
2019-12-13 15:48:30 +0100

    test

--- php/lvlchg.php
+++ php/lvlchg.php
@@ -1,34 +1,56 @@
 <?php
-@session_start();
-
-$db = new SQLite3('/www/temp/control4.db') or die('Unable to open database');
-
-if($_SESSION['lvl']==0){
-    if(isset($_GET['lvl']) && isset($_GET['id'])){
-
-        $statement = $db->prepare('SELECT * FROM C4users WHERE id=?;');
-        $statement->bindValue(1, $_GET['id'], SQLITE3_INTEGER);
-        $result = $statement->execute();
-        $row = $result->fetchArray();
-
-        if($_GET['lvl'] == "1"){
-            if($row['lvl'] > 0){
-                $statement = $db->prepare('UPDATE C4users SET lvl=? WHERE id=?;');
-                $statement->bindValue(1, $row['lvl'] - 1, SQLITE3_INTEGER);
-                $statement->bindValue(2, $_GET['id'], SQLITE3_INTEGER);
-                $result = $statement->execute();
-            }
-
-        }elseif($_GET['lvl'] == "2"){
-            if($row['lvl'] < 3){
-                $statement = $db->prepare('UPDATE C4users SET lvl=? WHERE id=?;');
-                $statement->bindValue(1, $row['lvl'] + 1, SQLITE3_INTEGER);
-                $statement->bindValue(2, $_GET['id'], SQLITE3_INTEGER);
-                $result = $statement->execute();
-            }
-        }
-    }
-}
-
-header("location:usrManage.php?dbRead=1");
+		@session_start();
 ?>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html;" charset=UTF-8”>
+        <title>Login page</title>
+        
+        <link rel="stylesheet" type="text/css" href="../CSS/stil1.css">
+
+    </head>
+<body>
+    <div class="main_div" id="div1">
+        <header> 
+            <div class="logo" id="logo" align="center">
+                <img src="../control4.png" />
+            </div>
+        </header>
+        <div class="body_div" id="body_div">
+            <h1 align="center" title="Name of the page">Manage users
+                <a href="top"></a>
+            </h1>
+            <div id="nav"> <table >
+                  <tr>
+                      <td>
+                          <ul> 
+                            <a href="uploadFile.php">
+                              <li>Upload file</li>
+                            </a>
+                          </ul>
+                      </td>
+                      <td >
+                          <ul>
+                              <a href="delFile.php">
+                                  <li>Delete file</li>
+                              </a>
+                          </ul>
+                      </td>
+                      <td >
+                          <ul>
+                              <a href="../index.php">
+                                  <li>Home page</li>
+                              </a>
+                          </ul>
+                      </td>
+                  </tr>
+              </table>
+            </div>
+
+            <div class="wrapper">
+
+            <p  id=return><a href="#top">Top of the page</a></p>a
+        </div>
+    </div>
+</body>
+</html>
