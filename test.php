6fd2c0407897e0f51dce16a9ec7aab9bcda6025c
hasan <hasanredzo@gmail.com>
2019-12-16 10:30:09 +0100

    test

--- test.php
+++ test.php
@@ -6,6 +6,16 @@ $dir    = '/mnt/sda1/test/test2/test3';
 // positive limit
 print_r(explode('/', $dir));
 
+$subdir = explode('/', $dir, -1);
+
 // negative limit (since PHP 5.1)
 print_r(explode('/', $dir, -1));
+
+$new_dir = "/";
+
+foreach ($subdir as $value) {
+    $new_dir = $new_dir."/".$value;
+}
+
+
 ?>
