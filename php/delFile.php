827d7d9b8fd6757f7ecd78f1edde8aa34a350853
hasan <hasanredzo@gmail.com>
2019-12-16 16:16:17 +0100

    test

--- php/delFile.php
+++ php/delFile.php
@@ -1,27 +1,31 @@
 <?php 
 @session_start();
 
-$dir_cur = $_SESSION['cur_path'];
+if(isset($_SESSION['lvl']) && $_SESSION['lvl'] < 2){
+    $dir_cur = $_SESSION['cur_path'];
 
-$dir_access = explode('/mnt/sda1/', $dir_cur);
+    $dir_access = explode('/mnt/sda1/', $dir_cur);
 
-$dir = "../usb/";
+    $dir = "../usb/";
 
-if($dir_access[1] != ""){
-    $dir = $dir.$dir_access[1]."/";
+    if($dir_access[1] != ""){
+        $dir = $dir.$dir_access[1]."/";
+    }
+
+    if(isset($_GET['flName'])){
+
+        $flName = $_GET['flName'];  
+        
+        $fl = $dir.$flName;
+
+        if (!unlink($fl)) {  
+            echo ("$fl cannot be deleted due to an error");  
+        }  
+        else {  
+            echo ("$fl has been deleted");  
+        }  
+    }
 }
 
-if(isset($_GET['flName'])){
-
-    $flName = $_GET['flName'];  
-    
-    $fl = $dir.$flName;
-
-    if (!unlink($fl)) {  
-        echo ("$fl cannot be deleted due to an error");  
-    }  
-    else {  
-        echo ("$fl has been deleted");  
-    }  
-}  
+header("location:usbSearch.php");
 ?>  
