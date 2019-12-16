27e05a4ee321a030cd4c6403d9b05dc979df523f
hasan <hasanredzo@gmail.com>
2019-12-16 10:24:11 +0100

    test

--- test.php
+++ test.php
@@ -1,10 +1,10 @@
 
 <?php
 
-$dir    = '/mnt/sda1/test';
+$dir    = '/mnt/sda1/test/test2/test3';
 
 // positive limit
-print_r(explode('/', $dir, 1));
+print_r(explode('/', $dir));
 
 // negative limit (since PHP 5.1)
 print_r(explode('/', $dir, -1));
