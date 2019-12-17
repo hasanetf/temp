e187c106c3be19efbdf795567f0441e9018fd094
hasan <hasanredzo@gmail.com>
2019-12-17 17:46:14 +0100

    test

--- php/readData.php
+++ php/readData.php
@@ -1,7 +1,13 @@
 <?php
 print "date,close\n";
 $db = new SQLite3('/www/temp/control4.db') or die('Unable to open database');
-$statement = $db->prepare('(SELECT * FROM temp ORDER BY id DESC LIMIT 50) ORDER BY id;');
+$statement = $db->prepare('SELECT count(*) FROM temp');
+$result = $statement->execute();
+$row = $result->fetchArray();
+$total = $row[0];
+
+$statement = $db->prepare('SELECT * FROM temp LIMIT 50 OFFSET ?-50');
+$statement->bindValue(1, $total, SQLITE3_INTEGER);
 $result = $statement->execute();
 
 while ($row = $result->fetchArray()) {
